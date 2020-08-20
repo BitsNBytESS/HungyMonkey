dea3a57096987479c470c98c15099d6b317e280f
SanjaySantokee <49313956+SanjaySantokee@users.noreply.github.com>
2020-08-19 23:20:40 -0400

    Added Routing

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Home from "./Home";
 
 const App = () => {
     return (
         <div className="ui container">
-            APP
+            <BrowserRouter>
+                <div>
+                    <Switch>
+                        <Route path="/" exact component={Home} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
         </div>
     );
 }
